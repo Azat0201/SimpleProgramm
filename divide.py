87e97472236b6b71c3e515fc9d07370de2390d16
Azat0201 <azatismanskij@gmail.com>
2025-02-09 23:06:30 +0300

    commit

---
+++ divide.py
@@ -0,0 +1,48 @@
+from re import fullmatch
+from math import ceil
+
+
+def sprint(*args):  # simple with separator and end is nothing
+    print(*args, sep='', end='')
+
+
+def is_correct_number(number):
+    if fullmatch(r'-?\d+/?-?\d+', number):
+        num1, num2 = map(lambda x: abs(int(x)), number.split('/'))
+        if num2 == 0:
+            raise ZeroDivisionError('Division by zero')
+        return num1, num2
+    elif fullmatch(r'-?\d+', number):
+        return abs(int(number)), 1
+    else:
+        raise ValueError('Wrong number')
+
+
+def do_fraction_is_correct(num):
+    num = num.lower().replace(':', '/')
+    if num == 'exit':
+        exit()
+    num1, num2 = is_correct_number(num)
+
+    sign = '-' if num.count('-') % 2 == 1 else ''
+    fraction_is_correct = False
+
+    sprint(num)
+    while not fraction_is_correct:
+        for i in range(2, ceil(min(num1, num2)**0.5) + 1):
+            if num1 % i == 0 and num2 % i == 0:
+                num1 //= i; num2 //= i
+                sprint(' = ', sign, num1, '/', num2)
+                break
+        else:
+            fraction_is_correct = True
+
+    if num1 >= num2 or num1 == 0:
+        sprint(' = ')
+        if num1 % num2 == 0:
+            sprint(num1 // num2)
+        else:
+            sprint(num1 // num2, ' ', num1 % num2, '/', num2)
+
+    print()
+
---
+++ divide_wrapper.py
@@ -0,0 +1,17 @@
+from divide import do_fraction_is_correct
+
+while True:
+    print('Write fractions in N1/N2 format, where N1 and N2 - integers, and N2 isn\'t zero:dfgd')
+    filepath = r'C:\Users\User\PycharmProjects\TEST\divide.py'
+    try:
+        do_fraction_is_correct(input())
+    except ValueError:
+        print('Wrong Number')
+    except ZeroDivisionError:
+        print('Division by zero')
+    except (FileNotFoundError, FileExistsError):
+        print('File not found')
+        break
+    except BaseException as error:
+        print(error)
+        break
